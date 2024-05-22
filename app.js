ed64374b99cb314c0ddd2554a13c4abbb32952ff
nd-jeremias <nd.jeremias@live.com>
2024-05-22 12:05:54 -0300

    Se ordeno MVC agregando ejs, y rutas dinamicas

--- app.js
+++ app.js
@@ -2,9 +2,28 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// /* Estaticos */ 
-
+/* Archivos Estaticos */ 
 app.use(express.static('public'));
 
+//app.get('./login')
 
-app.listen(PORT, () => console.log(`Servidor corriendo desde el puerto: ${PORT}`))
+/* View Engine */
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/src/views');
+
+/* Routes */
+app.get('/', (req, res) => {
+    res.render('home', {
+        tittle: 'CAC Home',
+        style: '../styles/style.css'
+    });
+});
+
+app.get('/login', (req, res) => {
+    res.render('login', {
+        tittle: 'CAC Login',
+        style: '../styles/loginstyle.css'
+    });
+});
+
+app.listen(PORT, () => console.log(`Servidor corriendo desde : http://localhost:${PORT}`))
